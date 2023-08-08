bc85cd9d9022b1097cff046af54dc9d99830fab5
Murat Teke <dr.muratteke@gmail.com>
2023-08-09 00:51:17 +0300

    230809-ForLoops

--- src/day14_forLoops/C01_SifreKontrolu.java
+++ src/day14_forLoops/C01_SifreKontrolu.java
@@ -6,13 +6,13 @@ public class C01_SifreKontrolu {
 
     public static void main(String[] args) {
 
-        //Soru 4 : Kullanicidan bir sifre isteyip, asagidaki sartlari kontrol edin
-        //         ve kullaniciya duzeltmesi gereken tum eksikleri soyleyin,
-        //         eger tum sartlari saglarsa, "sifre basariyla kaydedildi" yazdirin
-        //         - ilk harf kucuk harf olmali
-        //         - son karakter rakam olmali
-        //         - sifre bosluk icermemeli
-        //         - uzunlugu en az 10 karakter olmali
+        //  Kullanicidan bir sifre isteyip, asagidaki sartlari kontrol edin
+        //  ve kullaniciya duzeltmesi gereken tum eksikleri soyleyin,
+        //  eger tum sartlari saglarsa, "sifre basariyla kaydedildi" yazdirin
+        //      - ilk karakter kucuk harf olmali
+        //      - son karakter rakam olmali
+        //      - sifre bosluk icermemeli
+        //      - uzunlugu en az 10 karakter olmali
 
         // flag yontemini kullanalim
         // flag'i basta true yapalim, her uygun olmayan durumda false degeri atayalim
@@ -21,50 +21,44 @@ public class C01_SifreKontrolu {
 
         // sartlar birbirinden bagimsiz oldugundan bagimsiz if cumleleri kullanalim
 
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Lutfen sifrenizi giriniz");
         String sifre = scanner.nextLine();
 
-        //         - ilk harf kucuk harf olmali
+        //  - ilk harf kucuk harf olmali
+
         char ilkHarf = sifre.charAt(0);
-
         if (!(ilkHarf>='a' && ilkHarf<='z')){
-
-            System.out.println("Ilk karakter kucuk harf olmali");
+            System.out.println("ilk karakter kucuk harf olmali");
             flag = false;
         }
 
-
-        //         - son karakter rakam olmali
+        //  - son karakter rakam olmali
 
         char sonKarakter = sifre.charAt(sifre.length()-1);
 
         if (!(sonKarakter>='0' && sonKarakter<='9')){
-            System.out.println("Son karakter rakam olmali");
+            System.out.println("son karakter rakam olmali");
             flag=false;
         }
 
-
-        //         - sifre bosluk icermemeli
+        //  - sifre bosluk icermemeli
 
         if (sifre.contains(" ")){
-            System.out.println("Sifre bosluk icermemeli");
+            System.out.println("sifre bosluk icermemeli");
             flag = false;
         }
 
-        //         - uzunlugu en az 10 karakter olmali
+        //  - uzunlugu en az 10 karakter olmali
 
         if (!(sifre.length()>=10)){
-            System.out.println("Sifrenin uzunlugu en az 10 karakter olmali");
+            System.out.println("sifrenin uzunlugu en az 10 karakter olmali");
             flag=false;
         }
-
 
         if (flag){
             System.out.println("Sifre basarili sekilde kaydedildi");
         }
-
 
     }
 }
--- src/day14_forLoops/C03_ForLoop.java
+++ src/day14_forLoops/C03_ForLoop.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class C03_ForLoop {
     public static void main(String[] args) {
 
-        //Soru 4- Kullanicidan baslangic ve bitis degeri olarak pozitif sayilar alin,
-        //        sinirlar dahil olarak aralarindaki tum sayilarin toplamini yazdirin.
-        //        Bitis degeri baslangic degerinden kucuk olsa da program calissin
+        // Soru 4-  Kullanicidan baslangic ve bitis degeri olarak pozitif sayilar alin,
+        //          sinirlar dahil olarak aralarindaki tum sayilarin toplamini yazdirin.
+        //          Bitis degeri baslangic degerinden kucuk olsa da program calissin
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Lutfen baslangic degeri icin pozitif bir tamsayi giriniz");
         int baslangic= scanner.nextInt();
 
@@ -19,19 +20,15 @@ public class C03_ForLoop {
         int toplam = 0 ;
 
         if (baslangic < bitis){
-
             for (int i = baslangic; i <=bitis ; i++) {
                 toplam += i;
             }
-
         }else {
-
             for (int i = bitis; i <=baslangic ; i++) {
                 toplam += i ;
             }
-
         }
-
         System.out.println("Sinirlar arasindaki sayilarin toplami : " + toplam);
+
     }
 }
